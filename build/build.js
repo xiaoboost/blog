b72cfe30c9857f5139e1275d7d464ee6ca7e35bc
Xiao <xiaoboost@163.com>
2017-02-27 01:23:37 +0800

    补充生产环境的注释

--- build/build.js
+++ build/build.js
@@ -1,35 +1,37 @@
-require('./check-versions')()
+require('./check-versions')();
+// 运行当前文件一定是生产环境
+process.env.NODE_ENV = 'production';
 
-process.env.NODE_ENV = 'production'
+const // 一个比较漂亮的loading界面
+    ora = require('ora'),
+    // node下的删除命令，效果类似于 "rm -rf"
+    rm = require('rimraf'),
+    path = require('path'),
+    chalk = require('chalk'),
+    webpack = require('webpack'),
+    config = require('../config'),
+    // 读取生产环境的配置
+    webpackConfig = require('./webpack.prod.conf'),
+    spinner = ora('building for production...');
 
-var ora = require('ora')
-var rm = require('rimraf')
-var path = require('path')
-var chalk = require('chalk')
-var webpack = require('webpack')
-var config = require('../config')
-var webpackConfig = require('./webpack.prod.conf')
+spinner.start();
+rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), (err) => {
+    if (err) throw err;
+    webpack(webpackConfig, (err, stats) => {
+        spinner.stop();
+        if (err) throw err;
+        process.stdout.write(stats.toString({
+            colors: true,
+            modules: false,
+            children: false,
+            chunks: false,
+            chunkModules: false
+        }) + '\n\n');
 
-var spinner = ora('building for production...')
-spinner.start()
-
-rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
-  if (err) throw err
-  webpack(webpackConfig, function (err, stats) {
-    spinner.stop()
-    if (err) throw err
-    process.stdout.write(stats.toString({
-      colors: true,
-      modules: false,
-      children: false,
-      chunks: false,
-      chunkModules: false
-    }) + '\n\n')
-
-    console.log(chalk.cyan('  Build complete.\n'))
-    console.log(chalk.yellow(
-      '  Tip: built files are meant to be served over an HTTP server.\n' +
-      '  Opening index.html over file:// won\'t work.\n'
-    ))
-  })
-})
+        console.log(chalk.cyan('  Build complete.\n'));
+        console.log(chalk.yellow(
+            '  Tip: built files are meant to be served over an HTTP server.\n' +
+            '  Opening index.html over file:// won\'t work.\n'
+        ));
+    });
+});
--- build/check-versions.js
+++ build/check-versions.js
@@ -1,6 +1,7 @@
 // 该模块的作用是检查当前node和npm的版本，假如版本不符合标准那么会直接退出进程
 
 const chalk = require('chalk'),
+    // 版本号的语义格式化工具
     semver = require('semver'),
     packageConfig = require('../package.json');
 
@@ -11,11 +12,13 @@ function exec(cmd) {
 const versionRequirements = [
     {
         name: 'node',
+        // 子进程查询node版本
         currentVersion: semver.clean(process.version),
         versionRequirement: packageConfig.engines.node
     },
     {
         name: 'npm',
+        // 子进程查询npm版本
         currentVersion: exec('npm --version'),
         versionRequirement: packageConfig.engines.npm
     }
@@ -35,7 +38,7 @@ module.exports = function() {
 
     if (warnings.length) {
         console.log('');
-        console.log(chalk.yellow('To use this template, you must update following to modules:'))
+        console.log(chalk.yellow('To use this template, you must update following to modules:'));
         console.log();
         for (const i = 0; i < warnings.length; i++) {
             const warning = warnings[i];
