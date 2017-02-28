aa3c5dfe3d07715db7087780ab0e87c304eb0786
Xiao <xiaoboost@163.com>
2017-02-28 23:06:06 +0800

    完成页首和页脚的布局，js规则改为退2格

---
+++ src/components/SIteFooter.vue
@@ -0,0 +1,62 @@
+<template>
+  <footer>
+    <span>Powered by </span>
+    <a href="/">Xiao</a>
+    <span> © 2014 - 2016</span>
+    <div id="goto-up" style="opacity: 1;"></div>
+  </footer>
+</template>
+
+<script>
+
+</script>
+
+<style lang="stylus" scoped>
+@import '../css/variable'
+
+footer
+  font-family font-title-family
+  font-size 100%
+  font-weight bold
+  margin 3em 0 1em 0
+  text-align center
+  width 100%
+  color color-font
+  text-align center
+  flex 0 0 auto
+  a
+    color color-black
+    transition color 200ms ease-out
+    &:hover
+      color color-orange
+
+footer > div#goto-up
+  position fixed
+  bottom 2em
+  right 2em
+  display flex
+  flex-direction column-reverse
+  align-items center
+  height 3.5em
+  width 3.5em
+  border-radius 50%
+  color #FFFFFF
+  box-shadow 0px 2px 3px rgba(0, 0, 0, 0.5)
+  background #E53935
+  transition opacity 300ms linear
+  opacity 0
+  &:hover
+    box-shadow 0px 3px 5px rgba(0, 0, 0, 0.5)
+    cursor pointer
+  &:before
+    font-family font-icon-family
+    content "\f102"
+    font-size 230%
+  @media phone
+    bottom 1.6em
+    right 1.6em
+    height 2.8em
+    width 2.8em
+    &:before
+      font-size 180%
+</style>
