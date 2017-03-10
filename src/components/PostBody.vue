8abfa217f3581237a43c9b1d23c9d6c45770c40f
Xiao <xiaoboost@163.com>
2017-03-11 00:33:56 +0800

    细节修复，文章页面跳转完成

---
+++ src/components/PostBody.vue
@@ -0,0 +1,18 @@
+<template>
+  <article id="container">
+    <div class="post" id="main">
+      测试
+    </div>
+    <aside>
+      测试
+    </aside>
+  </article>
+</template>
+
+<script>
+
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
