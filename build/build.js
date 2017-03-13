852f7f62332179da638ee1aac175f51b96c0ae37
Xiao <xiaoboost@163.com>
2017-03-13 22:51:11 +0800

    修正生成产品时的路径问题

--- build/build.js
+++ build/build.js
@@ -6,16 +6,23 @@ const // 一个比较漂亮的loading界面
   ora = require('ora'),
   // node下的删除命令，效果类似于 "rm -rf"
   rm = require('rimraf'),
+  // node下创建文件夹的命令，效果类似于 "mkdir -p"
+  mkdirp = require('mkdirp'),
   path = require('path'),
+  fs = require('fs'),
   chalk = require('chalk'),
   webpack = require('webpack'),
   config = require('../config'),
+  // 生成博客网站
+  site = require('./create-site'),
   // 读取生产环境的配置
   webpackConfig = require('./webpack.prod.conf'),
-  spinner = ora('building for production...');
+  spinner = ora('building for production...'),
+  // 输出文件路径
+  output = path.join(config.build.assetsRoot, config.build.assetsSubDirectory);
 
 spinner.start();
-rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), (err) => {
+rm(output, (err) => {
   if (err) throw err;
   webpack(webpackConfig, (err, stats) => {
     spinner.stop();
@@ -27,6 +34,15 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), (err) =>
       chunks: false,
       chunkModules: false
     }) + '\n\n');
+
+    for (const i in site) {
+      const out = path.join(output, 'api', i);
+      mkdirp(path.dirname(out), (err) => {
+        debugger;
+        if (err) console.error(err);
+        fs.writeFileSync(out, JSON.stringify(site[i]));
+      });
+    }
 
     console.log(chalk.cyan('  Build complete.\n'));
     console.log(chalk.yellow(
