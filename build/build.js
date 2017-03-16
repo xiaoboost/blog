ba07af5e639eb9725d29c1476c82426febea4b54
Xiao <xiaoboost@163.com>
2017-03-17 00:05:04 +0800

    完成自动部署

--- build/build.js
+++ build/build.js
@@ -13,40 +13,86 @@ const // 一个比较漂亮的loading界面
     chalk = require('chalk'),
     webpack = require('webpack'),
     config = require('../config'),
+    // git子进程
+    git = require('./deployer'),
     // 生成博客网站
     site = require('./create-site'),
     // 读取生产环境的配置
     webpackConfig = require('./webpack.prod.conf'),
-    spinner = ora('building for production...'),
+    // 生成文件的进度条
+    build = ora('building for production...'),
+    // 上传文件的进度条
+    deploy = ora('deploying to site...'),
     // 输出文件路径
-    output = path.join(config.build.assetsRoot, config.build.assetsSubDirectory);
+    output = path.join(config.build.assetsRoot, config.build.assetsSubDirectory),
+    // 输入参数，默认为上传时间
+    message = process.argv[2] || (new Date()).toLocaleString(),
+    // 上传设定
+    url = require('../config/site').deploy.url,
+    branch = require('../config/site').deploy.branch;
 
-spinner.start();
-rm(output, (err) => {
-    if (err) throw err;
-    webpack(webpackConfig, (err, stats) => {
-        spinner.stop();
-        if (err) throw err;
-        process.stdout.write(stats.toString({
-            colors: true,
-            modules: false,
-            children: false,
-            chunks: false,
-            chunkModules: false
-        }) + '\n\n');
+// 构建进度条开始
+build.start();
+// 异步开始
+let status = Promise.resolve();
+// 生成本地文件
+status = status.then(() => {
+    // 删除成品文件夹中除[git文件夹]之外的所有东西
+    const files = fs.readdirSync(output)
+        .filter((file) => file !== '.git')
+        .map((file) => new Promise((res) => rm(path.join(output, file), res)));
 
-        for (const i in site) {
-            const out = path.join(output, 'api', i);
+    return Promise.all(files);
+}).then(() => {
+    // 生成所有API文件
+    const files = [];
+    for (const i in site) {
+        const out = path.join(output, 'api', i);
+        files.push(new Promise((res) => {
             mkdirp(path.dirname(out), (err) => {
                 if (err) console.error(err);
-                fs.writeFileSync(out, JSON.stringify(site[i]));
+                fs.writeFile(out, JSON.stringify(site[i]), res);
             });
-        }
+        }));
+    }
 
-        console.log(chalk.cyan('  Build complete.\n'));
-        console.log(chalk.yellow(
-            '  Tip: built files are meant to be served over an HTTP server.\n' +
-            '  Opening index.html over file:// won\'t work.\n'
-        ));
+    return Promise.all(files);
+}).then(() => {
+    // webpack打包网站
+    return new Promise((res, rej) => {
+        webpack(webpackConfig, (err, stats) => {
+            // 构建进度条结束
+            build.stop();
+            if (err) rej(err);
+            console.log(chalk.cyan('  Build complete.\n'));
+
+            res();
+        });
     });
+}).then(() => {
+    // 复制CNAME文件
+    return new Promise((res, rej) => {
+        fs.readFile(path.join(__dirname, '../CNAME'), (e, data) => {
+            if (e) rej(e);
+            fs.writeFile(path.join(output, 'CNAME'), data, res);
+        });
+    });
+});
+
+// 上传文件
+status
+    .then(() => Promise.resolve(deploy.start()))
+    .then(git('init', { cwd: output }))
+    .then(git('add', '-A'))
+    .then(git('commit', '-m', message))
+    .then(git('push', '-u', url, 'master:' + branch, '--force'))
+    .then(() => {
+        deploy.stop();
+        console.log(chalk.green('\n INFO: ') + '文件上传完毕');
+    });
+
+// 错误捕获
+status.catch((e) => {
+    console.log(chalk.red('\n ERROR: ') + '发生错误，意外中止\n');
+    console.error(chalk.red('\n 错误信息: ') + e);
 });
