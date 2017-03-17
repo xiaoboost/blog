e7e241c353153d1337e88bda9c91feeba67ee4a6
Xiao <xiaoboost@163.com>
2017-03-17 20:28:36 +0800

    网站模式更改为hash

--- src/router/index.js
+++ src/router/index.js
@@ -10,7 +10,7 @@ import PostBody from '../views/PostBody';
 Vue.use(Router);
 
 export default new Router({
-    mode: 'history',
+    mode: 'hash',  // history
     routes: [
         // 主页
         { path: '/index', alias: '/', name: 'index', component: IndexBody,
