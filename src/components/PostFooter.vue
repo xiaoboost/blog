eb343373405953c0b5e6ace02161e7eb2afbb1b8
Xiao <xiaoboost@163.com>
2017-04-03 21:50:48 +0800

    主页调整

--- src/components/PostFooter.vue
+++ src/components/PostFooter.vue
@@ -31,23 +31,28 @@ export default {
 
 footer.post-footer
   margin 0
-  padding 0 4%
-  line-height 2.5em
+  padding .5em 1.5em
+  line-height 1em
   border-top 1px solid color-border
+  @media mini
+    padding .5em 1em
   span
     color #d6d6d6
-    margin-right 2em
+    display inline-block
+    margin .25em 2em .25em 0
     &:before
       font-family "FontAwesome"
       color #999
       margin-right 0.2em
       font-smoothing()
   span:nth-child(1)
+    margin-top 0
     &:before
       content "\f07b"
     a
       margin 0 0.3em
   span:nth-child(2)
+    margin-bottom 0
     &:before
       content "\f02c"
     a
