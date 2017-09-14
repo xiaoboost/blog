7858605dd50690d1e88f1d7db55e4335eb922144
Xiao <xiaoboost@163.com>
2017-09-14 21:03:02 +0800

    修复侧边栏滚动跟随的问题

--- src/directives/followscroll.js
+++ src/directives/followscroll.js
@@ -4,7 +4,9 @@
  *   bias 界线，即距离网页顶端的距离
  *   callback 回调函数，当“是否超过界线”的状态改变时触发
  */
-const body = document.body;
+
+// 全局回调函数缓存
+const cache = new Map();
 
 export default {
     bind(el, binding, vnode) {
@@ -15,19 +17,23 @@ export default {
         // 保存当前状态，默认为false
         let status = false;
         // 绑定scroll事件回调
-        el.__vueFollowScroll__ = function() {
+        function followScroll() {
             // 只有当状态发生改变时才会触发回调
-            if ((body.scrollTop > bias) !== status) {
-                status = (body.scrollTop > bias);
+            const offsetTop = document.documentElement.scrollTop;
+            if ((offsetTop > bias) !== status) {
+                status = (offsetTop > bias);
                 callback(status);
             }
-        };
-        document.addEventListener('scroll', el.__vueFollowScroll__);
+        }
+        document.addEventListener('scroll', followScroll);
+        cache.set(el, followScroll);
         // 绑定之后马上运行一次事件
-        el.__vueFollowScroll__();
+        followScroll();
     },
     unbind(el) {
-        document.removeEventListener('scroll', el.__vueFollowScroll__);
-        delete el.__vueFollowScroll__;
+        if (!cache.has(el)) { return; }
+
+        el.removeEventListener('click', cache.get(el));
+        cache.delete(el);
     }
 };
