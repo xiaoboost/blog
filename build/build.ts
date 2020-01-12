22d4ba4cd3c0b024049e40155ac0399b61f64ad2
xiaoboost <xiaoboost@qq.com>
2020-01-13 02:07:41 +0800

    layout init

---
+++ build/build.ts
@@ -0,0 +1,7 @@
+import { render } from './renderer';
+
+async function main() {
+    render();
+}
+
+main();
---
+++ template/components/layout/index.tsx
@@ -0,0 +1,24 @@
+import { default as React, PropsWithChildren } from 'react';
+
+import { Header } from '../header';
+import { Footer } from '../footer';
+
+export function Layout({ children }: PropsWithChildren<{}>) {
+    return (
+        <html lang='cmn-Hans-CN'>
+            <head>
+                <meta name="charset" content="utf-8" />
+                <meta name='X-UA-Compatible' content='IE=edge' />
+                <meta name='viewport' content='width=device-width, initial-scale=1' />
+                <link rel='short icon' href='<% publicPath %>/img/favicon.ico' />
+            </head>
+            <body>
+                <Header />
+                <article>
+                    {children}
+                </article>
+                <Footer />
+            </body>
+        </html>
+    );
+}
