51c375e7d3e92cbfda666dcf0ac1f9f80590a6ea
xiaoboost <xiaoboost@qq.com>
2020-01-22 15:37:31 +0800

    set config

---
+++ config/project.ts
@@ -0,0 +1,13 @@
+import { resolve } from '../build/utils';
+
+/** 项目输出文件路径 */
+export const buildOutput = resolve('dist');
+/** 网站资源公共路径 */
+export const publicPath = '/';
+/** 网站地址 */
+export const website = 'www.dreamingcat.me';
+
+/** 全局引用的样式文件 */
+export const styleFile = 'css/style.css';
+/** 全局引用的脚本文件 */
+export const scriptFile = 'js/script.js';
