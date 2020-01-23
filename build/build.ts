26ef9a5eb2b193990b7285c886c8e7f9ae5ebbdd
xiaoboost <xiaoboost@qq.com>
2020-01-23 17:08:29 +0800

    add plugin and tokens entry

--- build/build.ts
+++ build/build.ts
@@ -1,7 +1,13 @@
+import { mkdirp } from 'fs-extra';
+
 import { render } from './renderer';
+import { build as buildAssets } from './assets';
+import { buildOutput } from '../config/project';
 
 async function main() {
-    render();
+    await mkdirp(buildOutput);
+    await buildAssets();
+    await render();
 }
 
 main();
--- template/views/default/index.tsx
+++ template/views/default/index.tsx
@@ -1,32 +1,42 @@
 import React from 'react';
 
+import { markdown } from 'build/markdown';
 import { Layout } from 'template/components/layout';
 
 interface Props {
     /** 项目公共配置 */
     project: {
         publicPath: string;
-        stylePath: string;
-        scriptPath: string;
+        styleFile: string;
+        scriptFile: string;
     },
-
-    /** 当前文章标题 */
-    title: string;
     /** 文章内容 */
-    content: string;
-    /** 创建时间 */
-    create: number;
-    /** 最后更新时间 */
-    update: number;
+    post: {
+        /** 当前文章标题 */
+        title: string;
+        /** 文章原内容 */
+        content: string;
+        /** 创建时间 */
+        create: number;
+        /** 最后更新时间 */
+        update: number;
+        /** 当前文章标签 */
+        tags: string[];
+        /** 插件列表 */
+        plugins: string[];
+    },
 }
 
-export function Template({ project, ...post }: Props) {
+export function Template({ project, post }: Props) {
+    const content = markdown.render(post.content);
+    const tokens = markdown.parse(post.content, {});
+
     return (
         <Layout {...{
             ...project,
             title: post.title,
         }}>
-            <div dangerouslySetInnerHTML={{ __html: post.content }} />
+            <div dangerouslySetInnerHTML={{ __html: content }} />
         </Layout>
     )
 }
