e49c884f4f2e38def47c9fcd4544727c177692e3
xiaoboost <xiaoboost@qq.com>
2020-03-23 02:20:13 +0800

    fix post transform

--- src/template/views/default/index.tsx
+++ src/template/views/default/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-// import { markdown } from 'build/renderer/markdown';
 import { Layout } from 'src/template/components/layout';
+import { PostData } from 'src/loader/post';
 
 /** 项目公共配置 */
 interface ProjectEv {
@@ -10,41 +10,18 @@ interface ProjectEv {
     scriptFile: string;
 }
 
-/** 文章数据 */
-interface PostData {
-    /** 当前文章标题 */
-    title: string;
-    /** 文章原内容 */
-    content: string;
-    /** 创建时间 */
-    date: number;
-    /** 最后更新时间 */
-    update: number;
-    /** 当前文章标签 */
-    tags: string[];
-    /** 插件列表 */
-    plugins: string[];
-    /** 上一篇文章 */
-    pre?: PostData;
-    /** 下一篇文章 */
-    next?: PostData;
-}
-
 interface Props {
     project: ProjectEv;
     post: PostData;
 }
 
 export function Template({ project, post }: Props) {
-    // const content = markdown.render(post.content);
-    // const tokens = markdown.parse(post.content, {});
-
     return (
         <Layout {...{
             ...project,
             title: post.title,
         }}>
-            <div dangerouslySetInnerHTML={{ __html: post.content }} />
+            <div dangerouslySetInnerHTML={{ __html: post.html }} />
         </Layout>
     )
 }
