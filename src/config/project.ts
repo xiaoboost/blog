e864273545893b2407404b4f2685f7189f354735
xiaoboost <xiaoboost@qq.com>
2020-03-27 23:52:50 +0800

    finish server

--- src/config/project.ts
+++ src/config/project.ts
@@ -2,6 +2,8 @@ import { resolveRoot } from '../utils/path';
 
 /** 项目输出文件路径 */
 export const buildOutput = resolveRoot('dist');
+/** 文章路径 */
+export const postsDir = resolveRoot('posts');
 
 /** 网站资源公共路径 */
 export const publicPath = '/';
