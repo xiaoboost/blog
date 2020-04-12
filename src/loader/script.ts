5e5dffe2bb855d5498ae7d536cde11d16a8bc485
xiaoboost <xiaoboost@qq.com>
2020-04-12 15:47:03 +0800

    fix base watch

--- src/loader/script.ts
+++ src/loader/script.ts
@@ -1,12 +1,9 @@
 import { BaseItem } from './base';
-import { scriptFile } from 'src/config/project';
 
 import * as path from 'path';
 import * as fs from 'fs-extra';
 
 export class ScriptItem extends BaseItem {
-    buildTo = scriptFile;
-
     static async Create() {
         // ..
     }
--- src/loader/style.ts
+++ src/loader/style.ts
@@ -1,15 +1,19 @@
 import less from 'less';
 
 import { BaseItem } from './base';
-import { styleFile } from 'src/config/project';
 
 import * as path from 'path';
 import * as fs from 'fs-extra';
 
-export class LessItem extends BaseItem {
-    buildTo = styleFile;
+// 全局 style 文件
+let style: StyleItem;
 
+export class StyleItem extends BaseItem {
     static async Create() {
+        if (style) {
+            return style;
+        }
+
         // ..
     }
 }
