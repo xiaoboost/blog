1a1df4355606d792efb08480cf87d386bbea9f97
xiaoboost <xiaoboost@qq.com>
2020-04-13 02:43:12 +0800

    fix base loader

--- src/loader/base.ts
+++ src/loader/base.ts
@@ -3,13 +3,10 @@ import * as fs from 'fs-extra';
 import * as fms from '../utils/memory-fs';
 
 import { buildOutput } from 'src/config/project';
-import { deleteVal, exclude } from 'src/utils/array';
 
-interface ErrorMessage {
-    id: number;
-    message: string;
-    position: string;
-}
+import { isEqual } from 'src/utils/object';
+import { isArray } from 'src/utils/assert';
+import { deleteVal, exclude } from 'src/utils/array';
 
 /** 文件系统 */
 const fileSystem = process.env.NODE_ENV === 'development' ? fms : fs;
@@ -17,22 +14,19 @@ const fileSystem = process.env.NODE_ENV === 'development' ? fms : fs;
 /** 资源列表 */
 export const sources: BaseLoader[] = [];
 
-/** 错误信息 */
-export const errors: ErrorMessage[] = [];
-
 /** 全局资源编号 */
 let id = 1;
 
 interface WatchData {
     depId: number;
     lastVal: any[];
-    keys: string[];
     notify(): any;
+    compute(item: BaseLoader): any;
 }
 
 interface DepData {
     dep: BaseLoader;
-    keys: string[];
+    compute(item: BaseLoader): any;
 }
 
 /** 元素类 */
@@ -44,15 +38,14 @@ export class BaseLoader {
     /** 相对于根目录的相对路径 */
     buildTo = '';
     /** 错误信息 */
-    errorMessage = ''; 
+    error = '';
+    /** 是否正在编译 */
+    transforming = false; 
 
     /** 元素源代码 */
     origin: Buffer | string = '';
     /** 转换之后元素内容 */
     source: Buffer | string = '';
-
-    /** 内部真实的转换器 */
-    private _transform: () => Promise<void>;
 
     static FindSource(from: string) {
         const exist = sources.find((image) => image.from === from);
@@ -66,8 +59,6 @@ export class BaseLoader {
 
     constructor(path: string) {
         this.from = path;
-        this._transform = async () => this.diffSources(await this.transform());
-
         sources.push(this);
     }
 
@@ -82,12 +73,12 @@ export class BaseLoader {
     }
 
     /** 监听某个属性 */
-    observe<T extends BaseLoader, K extends GetString<keyof T>>(this: T, dep: BaseLoader, keys: K[]) {
+    observe<T extends BaseLoader>(this: T, id: number, compute: (item: T) => any) {
         this._observers.push({
-            depId: dep.id,
+            compute,
+            depId: id,
             lastVal: [],
             notify: this._transform.bind(this),
-            keys,
         });
     }
 
@@ -100,9 +91,9 @@ export class BaseLoader {
     async notify() {
         for (const ob of this._observers) {
             const last = ob.lastVal;
-            const now = ob.keys.map((key) => this[key]);
+            const now = ob.compute(this);
 
-            if (last.some((val, i) => val !== now[i])) {
+            if (!isEqual(last, now)) {
                 ob.lastVal = now;
                 await ob.notify();
             }
@@ -110,12 +101,20 @@ export class BaseLoader {
     }
 
     /**
-     * 数据变换函数
-     *  - 派生类全部需要扩写这个函数，在此函数中需要返回引用的数据数组
+     * 外部数据变换函数
+     *  - 派生类全部需要扩写这个函数，可能的话，此函数可以返回引用的数据数组
      */
-    transform(): DepData[] {
+    transform(): void | DepData[] | Promise<void> | Promise<DepData[]> {
         this.source = this.origin;
-        return [];
+    }
+
+    /** 内部真实的转换器 */
+    private async _transform() {
+        const result = await this.transform();
+
+        if (isArray(result)) {
+            return this.diffSources(result);
+        }
     }
 
     /** 检测引用元素是否变更 */
@@ -124,7 +123,7 @@ export class BaseLoader {
         const inSource = exclude(deps, this._sources, ({ dep }) => dep.id);
 
         // 绑定新的引用
-        inDeps.forEach(({ dep, keys }) => dep.observe(this, keys as any[]));
+        inDeps.forEach(({ dep, compute }) => dep.observe(this.id, compute));
         // 解除不再引用的资源
         inSource.forEach(({ dep }) => dep.unObserve(this.id));
 
@@ -136,23 +135,20 @@ export class BaseLoader {
         // ..
     }
 
-    setError(err: ErrorMessage) {
-
-    }
-
-    clearError() {
-
+    /** 销毁资源 */
+    destroy() {
+        // ..
     }
 
     /** 此资源写入硬盘系统 */
     async write() {
         if (!this.buildTo) {
-            this.errorMessage = '未设置输出路径';
+            this.error = '未设置输出路径';
             return;
         }
 
-        // 非 html 文件，且引用计数为 0，则不输出
-        if (path.extname(this.buildTo) !== '.html' && this.quoteCount === 0) {
+        // 引用计数为 0，不输出
+        if (this.quoteCount === 0) {
             return;
         }
 
---
+++ src/template/plugins/goto-top/template.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function Viewer() {
+    return <div>goto top</div>;
+}
