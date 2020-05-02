bb7c2b74a2dca33bf367bfd252a83c6a51c1c23a
xiaoboost <xiaoboost@qq.com>
2020-05-02 22:13:27 +0800

    finish basic

--- src/template/views/post/default/index.tsx
+++ src/template/views/post/default/index.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
+import Moment from 'moment';
 
 import { PostData } from 'src/loader/post';
 import { Layout, LayoutProps } from 'src/template/components/layout';
 
-interface Props extends PostData, LayoutProps {
-    // ..
-}
+type Props = PostData & LayoutProps;
 
 export function Template(props: Props) {
     return (
         <Layout {...props}>
-            <div dangerouslySetInnerHTML={{ __html: props.html }} />
+            <section className='post-default'>
+                <header className='post-header'>
+                    <span className='post-header__title'>{props.title}</span>
+                    <time className='post-header__create'>{Moment(props.date).format('yyyy-MM-DD')}</time>
+                </header>
+                <article
+                    className='post-article'
+                    dangerouslySetInnerHTML={{ __html: props.html }}
+                />
+            </section>
         </Layout>
     )
 }
+ 
