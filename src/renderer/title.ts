36d7bdcd284183e058be27c053fb0f62574469c0
xiaoboost <xiaoboost@qq.com>
2020-05-10 22:54:19 +0800

    add title anchor

---
+++ src/renderer/title.ts
@@ -0,0 +1,18 @@
+import MarkdownIt from 'markdown-it';
+
+import { toPinyin } from 'src/utils/string';
+
+export function TitleRender(md: MarkdownIt) {
+    md.renderer.rules.heading_open = (tokens, idx, ops, env, self) => {
+        const head = tokens[idx];
+        const text = tokens[idx + 1];
+
+        if (!text || !text.content) {
+            return `<${head.tag}>`;
+        }
+
+        const id = toPinyin(text.content);
+
+        return `<${head.tag} id="${id}"><a class="anchor" href="#${id}">§</a>`;
+    };
+}
