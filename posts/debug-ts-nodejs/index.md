51e9526d2b10b59c7151f930d172d7ad6f1684a2
xiaoboost <xiaoboost@qq.com>
2020-05-23 22:05:40 +0800

    add code line highlight

--- posts/debug-ts-nodejs/index.md
+++ posts/debug-ts-nodejs/index.md
@@ -8,7 +8,7 @@ description: 总结了下现在在 VSCode 中调试 TypeScript 的三种方法�
 
 # 编译调试
 
-VSCode 的调试任务都需要在项目根目录中配置`.vscode/launch.json`文件作为调试任务的启动配置。这里实际上是直接调用 node 启动调试，`Nodejs`环境并不原生支持 ts 语言，所以在调试之前必须要先将 ts 代码编译成 js 代码。编译 ts 代码的方式就很多了，直接使用`tsc`或者是使用什么工具都可以。这种方式有个小缺点，就是编译和运行是分开的两个命令，没办法做到编译完成自动开始调试，需要自己手动点，就有点麻烦。这里就介绍以下 VSCode 的 preLaunchTask 功能，顾名思义它就是先运行某个命令再运行调试，这就可以达到编译完成后自动开始调试了。
+VSCode 的调试任务都需要在项目根目录中配置`.vscode/launch.json`文件作为调试任务的启动配置。由于`Nodejs`环境并不原生支持 ts 语言，所以在调试之前必须要先将 ts 代码编译成 js 代码。编译 ts 代码的方式就很多了，使用`tsc`或者是`rollup`之类的工具都可以，但是这种方式有个小缺点，就是编译和运行是分开的两个命令，没办法做到编译完成自动开始调试，需要自己手动点，就有点麻烦。这里要介绍的就是配置中的`preLaunchTask`选项，顾名思义它就是先运行某个命令再运行调试，这就可以达到编译完成后自动开始调试了。
 
 参考`.vscode/launch.json`配置：
 
@@ -22,7 +22,7 @@ VSCode 的调试任务都需要在项目根目录中配置`.vscode/launch.json`�
             "request": "launch",
             "sourceMaps": true,
             "stopOnEntry": true,
-            "preLaunchTask": "compile",
+            "preLaunchTask": "compile", /*** hl ***/
             "program": "${workspaceRoot}/out/index.js",
             "env": {
                 "NODE_ENV": "development"
@@ -32,4 +32,27 @@ VSCode 的调试任务都需要在项目根目录中配置`.vscode/launch.json`�
 }
 ```
 
-具体的可以参考文档：[launch.json](https://code.visualstudio.com/docs/editor/debugging#_launchjson-attributes)
+上面的代码中，将`preLaunchTask`指向了`compile`任务，这个任务就是我们的预编译任务。而 VSCode 的任务配置是放在`.vscode/tasks.json`文件中。这里以本地运行`tsc`命令为例，配置如下：
+
+```json
+{
+    "version": "2.0.0",
+    "tasks": [
+        {
+            "label": "compile", /*** hl ***/
+            "command": "node",
+            "args": [
+                "${workspaceRoot}/node_modules/typescript/lib/tsc.js",
+                "-p", ".",
+                "-m", "CommonJS",
+                "--sourceMap", "true",
+                "--outDir", "./out/"
+            ]
+        }
+    ]
+}
+```
+
+这里的预编译任务是启动本地`node_modules`中的`tsc`脚本，这和全局 tsc 命令是一样的。这个预编译任务运行完成之后，就会立即启动我们真正的调试任务。
+
+不过这种方式还是有缺陷，就是每次都要预编译，如果代码规模较大速度就会很慢，并且我们实际上调试的还是最后的 js 代码，原本在 ts 代码中的路径设置几乎都会失效，这就导致我们写代码就必须要全部使用相对路径
--- src/renderer/code.ts
+++ src/renderer/code.ts
@@ -16,17 +16,65 @@ const langLabel = {
     json: 'JSON',
 };
 
-export function CodeRenderer(code: string, lang: string) {
+function getSpaceWidth(str: string) {
+    const result = /^ +/.exec(str);
+    return result ? result[0].length : 0;
+}
+
+function getHighlightCode(code: string) {
+    const hlLabel = /\/\*\*\* +hl +\*\*\*\//g;
+    const highlightLine: Record<number, boolean> = {};
+    const lines = code.trim().split(/[\n\r]+/).map((line, index) => {
+        const match = line.match(hlLabel);
+
+        if (match) {
+            line = line.replace(hlLabel, '');
+            highlightLine[index] = true;
+        }
+
+        return line.trimRight();
+    });
+
+    return {
+        code: lines.join('\n'),
+        lines: highlightLine,
+    };
+}
+
+export function CodeRenderer(input: string, lang: string) {
     /** 代码语言 */
     const lan = lang ? lang.toLowerCase() : '';
     /** 代码语言标记 */
     const label = langLabel[lan];
+    /** 经处理的代码和高亮行 */
+    const { code, lines } = getHighlightCode(input);
     /** 渲染代码 */
     const text = lan ? highlight(lan, code) : highlightAuto(code);
-    /** 按照行切割代码 */
-    const content = text.value.trim().split('\n').map((n) => (`<li>${n}</li>`));
+    /** 按行切割代码 */
+    const codeLines = text.value.trim().split('\n');
+    /** 总共有多少前置空格 */
+    const totalSpace = codeLines.reduce((ans, line) => ans + getSpaceWidth(line), 0);
+    /** 空格宽度 */
+    const splitWidth = (totalSpace % 4 === 0) ? 4 : 2;
+    /** 空格字符串 */
+    const stringSpace = Array(splitWidth).fill(' ').join('');
+    /** 按照行编译代码 */
+    const content = codeLines.map((line, index) => {
+        const space = getSpaceWidth(line);
+        const number = space / splitWidth;
+        const spaceWithSplit = Array(number).fill(`<span class="code-block__split"></span>${stringSpace}`).join('');
+        const newLine = line.replace(/^ +/, spaceWithSplit);
+
+        return lines[index]
+            ? `<li class="code-block__highlight-line">${newLine}</li>`
+            : `<li>${newLine}</li>`;
+    });
     /** 代码行号 */
-    const list = new Array(content.length).fill(0).map((_, i) => (`<li>${(i + 1)}</li>`));
+    const list = new Array(codeLines.length).fill(0).map((_, i) => {
+        return lines[i]
+            ? `<li class="code-block__highlight-line">${i + 1}</li>`
+            : `<li>${i + 1}</li>`;
+    });
 
     return (
         `<pre class="code-block code-block__lang-${lan}">` +
