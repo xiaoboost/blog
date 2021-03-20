b062803a1b13f8fa62d413a7e6601925a46f1848
xiaoboost <xiaoboost@qq.com>
2021-03-21 00:43:35 +0800

    renderer init

--- packages/cli/src/process/index.ts
+++ packages/cli/src/process/index.ts
@@ -1,12 +1,15 @@
 import { cname } from './cname';
-import { buildTemplate } from './template';
 import { write } from './files';
+import { buildTemplate, Template } from './template';
 
 export async function build() {
+  let template: Template;
+
   await cname();
-  await buildTemplate((param) => {
-    debugger;
-    console.log(param);
+
+  template = await buildTemplate((param) => {
+    template = param;
   });
+
   await write();
 }
---
+++ packages/cli/src/process/renderer.ts
@@ -0,0 +1,14 @@
+import type { Template } from './template';
+import { push } from './files';
+
+function renderIndex(template: Template) {
+  // ..
+}
+
+function renderPost(template: Template) {
+  // ..
+}
+
+export function render(template: Template) {
+  // ..
+}
