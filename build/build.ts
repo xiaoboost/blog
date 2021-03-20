7c509e07d458080063b486a4d3f8edf22f778746
xiaoboost <xiaoboost@qq.com>
2021-03-21 04:40:00 +0800

    fix builder build

---
+++ build/build.ts
@@ -0,0 +1,11 @@
+import { build } from 'esbuild';
+import { resolveRoot, isDevelopment } from './utils';
+import { mergeConfig } from './process/utils';
+
+build(mergeConfig({
+  entryPoints: [resolveRoot('build/index.ts')],
+  outfile: resolveRoot('builder/index.js'),
+  write: true,
+  logLevel: 'info',
+  sourcemap: isDevelopment,
+}));
