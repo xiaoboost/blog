31f8f3e7576209d34689416282d792c23f338aba
xiaoboost <xiaoboost@qq.com>
2021-03-31 00:16:02 +0800

    finish info style

--- template/plugins/index.ts
+++ template/plugins/index.ts
@@ -1,7 +1,1 @@
-import './goto-top/style.styl';
-import './highlight/style.styl';
-import './ts-info/style.styl';
-import './to-content/style.styl';
-import './ketax/style.styl';
-
 export * from './goto-top/template';
--- template/plugins/ts-info/script.ts
+++ template/plugins/ts-info/script.ts
@@ -2,7 +2,7 @@ import { parseJSON } from '@build/utils/object';
 import { isString } from '@build/utils/assert';
 import { DisplaySymbol } from '@build/markdown/typescript';
 
-function createInfoDom(infos: DisplaySymbol[]) {
+function createInfoDom(infos: DisplaySymbol[]): HTMLElement {
   const infoSpan = infos.map((info) => {
     return isString(info)
       ? info
@@ -11,14 +11,14 @@ function createInfoDom(infos: DisplaySymbol[]) {
   const code = `<div class="ls-info-box"><pre>${infoSpan.join('')}</pre></div>`;
   const el = document.createElement('div');
   el.innerHTML = code;
-  return el.children[0];
+  return el.children[0] as HTMLElement;
 }
 
 Array.from(document.querySelectorAll<HTMLElement>('pre span[ls-info]')).forEach((el) => {
   const infoStr = el.getAttribute('ls-info') ?? '';
   const infoData = parseJSON(infoStr.replace(/'/g, '"'));
-  
-  el.removeAttribute('ls-info');
+
+  el.setAttribute('ls-info', '');
 
   if (!infoData) {
     return;
@@ -27,10 +27,14 @@ Array.from(document.querySelectorAll<HTMLElement>('pre span[ls-info]')).forEach(
   const infoEle = createInfoDom(infoData);
 
   el.addEventListener('mouseenter', () => {
-    // document.body.appendChild(infoEle);
+    const offset = el.getBoundingClientRect();
+
+    document.body.appendChild(infoEle);
+    infoEle.style.left = `${offset.left}px`;
+    infoEle.style.top = `${offset.top}px`;
   });
 
   el.addEventListener('mouseleave', () => {
-    // document.body.removeChild(infoEle);
+    document.body.removeChild(infoEle);
   });
 });
