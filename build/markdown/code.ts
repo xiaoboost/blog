d2c7bf5e7e200aa5221214af0941741ca75f5369
xiaoboost <xiaoboost@qq.com>
2021-05-01 21:25:58 +0800

    fix: fix style

--- build/markdown/code.ts
+++ build/markdown/code.ts
@@ -1,6 +1,15 @@
 import { highlight, highlightAuto } from 'highlight.js';
-import { renderTsCode, Platform, ScriptKind } from './typescript';
 import { stringifyClass } from '@build/utils';
+import { renderTsCode, Platform, ScriptKind } from './typescript';
+import { highlightLineClassName, lspClassName } from './constant';
+
+import {
+  createNumberList,
+  getLineSpaceWidth,
+  getMinSpaceWidth,
+  addSplitLabel,
+  getHighlightCode,
+} from './utils';
 
 const langLabel = {
   html: 'HTML',
@@ -19,95 +28,6 @@ const langLabel = {
   haskell: 'Haskell',
   json: 'JSON',
 };
-
-/** 代码有多少前置空格 */
-function getLineSpaceWidth(str: string) {
-  const result = /^ +/.exec(str);
-  return result ? result[0].length : 0;
-}
-
-/** 代码添加分割线 */
-function addSplitLabel(line: string, tabWidth: number) {
-  const splitCode = '<span class="code-block__split"></span>';
-
-  let code = line;
-  let index = 0;
-  let realIndex = 0;
-
-  if (line.trim().length === 0) {
-    return splitCode;
-  }
-
-  while (realIndex < code.length) {
-    if (code[realIndex] !== ' ') {
-      break;
-    }
-
-    if (index % tabWidth !== 0) {
-      index++;
-      realIndex++;
-      continue;
-    }
-
-    const before = code.substring(0, realIndex);
-    const after = code.substring(realIndex);
-
-    code = before + splitCode + after;
-
-    index++;
-    realIndex = realIndex + splitCode.length + 1;
-  }
-
-  return code
-}
-
-/** 获取当前代码 tab 宽度 */
-function getMinSpaceWidth(str: string) {
-  const result = str.trim().split('\n').reduce((ans, line) => {
-    const space = getLineSpaceWidth(line);
-
-    if (space <= 0) {
-      return ans;
-    }
-
-    if (ans === 0 || space < ans) {
-      return space;
-    }
-    else {
-      return ans;
-    }
-  }, 0);
-
-  const widths = [2, 4, 8, 16].sort((pre, next) => {
-    const preRes = Math.abs(pre - result);
-    const nextRes = Math.abs(next - result);
-
-    return preRes < nextRes ? -1 : 1;
-  });
-
-  return widths[0];
-}
-
-/** 获取高亮代码行记录 */
-function getHighlightCode(code: string) {
-  const hlLabel = /\/\*\*\* +hl +\*\*\*\//g;
-  const highlightLines: Record<number, boolean> = {};
-  const lines = code.trim().split(/[\n\r]/).map((line, index) => {
-    const match = line.match(hlLabel);
-
-    if (match) {
-      line = line.replace(hlLabel, '');
-      highlightLines[index] = true;
-    }
-
-    return line.trimRight();
-  });
-
-  return {
-    code: lines.join('\n'),
-    highlightLines,
-  };
-}
 
 function parseAttr(input: string) {
   const attrs: Record<string, string | boolean> = {};
@@ -134,6 +54,14 @@ function toScriptKind(input: string): ScriptKind | false {
   }
 }
 
+function renderCode(code: string, language: string, tabWidth: number) {
+  const rendered = highlight(code, { language });
+  const lines = rendered.value.trim().split('\n');
+  return lines.map((line) => {
+    return getLineSpaceWidth(line) > 0 ? addSplitLabel(line, tabWidth) : line;
+  });
+}
+
 export function CodeRenderer(input: string, lang: string, attribute = '') {
   /** 代码语言 */
   const lan = lang ? lang.toLowerCase() : '';
@@ -149,32 +77,18 @@ export function CodeRenderer(input: string, lang: string, attribute = '') {
   const { code, highlightLines } = getHighlightCode(input);
   /** 行代码 */
   const codeLines = scriptKind
-    ? renderTsCode(code, scriptKind, attrs.platform as Platform)
-    : (
-      lan
-        ? highlight(code, { language: lan })
-        : highlightAuto(code)
-    ).value.trim().split('\n');
-
-  /** 按照行编译代码 */
-  const content = codeLines.map((line, index) => {
-    const hasLeftSpace = getLineSpaceWidth(line) > 0;
-    const className = highlightLines[index] ? ` class="code-block__highlight-line"` : '';
-    const lineCode = hasLeftSpace
-      ? `<li${className}>${addSplitLabel(line, tabWidth)}</li>`
-      : `<li${className}>${line}</li>`;
-
-    return lineCode;
+    ? renderTsCode(code, tabWidth, scriptKind, attrs.platform as Platform)
+    : renderCode(code, lan, tabWidth);
+  /** 渲染后的行代码 */
+  const codeLineRendered = codeLines.map((line, i) => {
+    const className = highlightLines[i] ? ` class="${highlightLineClassName}"` : '';
+    return `<li${className}>${line}</li>`;
   });
   /** 代码行号 */
-  const list = new Array(codeLines.length).fill(0).map((_, i) => (
-    highlightLines[i]
-      ? `<li class="code-block__highlight-line">${i + 1}</li>`
-      : `<li>${i + 1}</li>`
-  ));
+  const list = createNumberList(codeLineRendered.length, highlightLines);
   /** 代码 class 名称 */
   const codeClassName = stringifyClass('code-block__code', {
-    'code-block__lsp': Boolean(scriptKind),
+    [lspClassName]: Boolean(scriptKind),
   });
 
   return (
@@ -183,7 +97,7 @@ export function CodeRenderer(input: string, lang: string, attribute = '') {
       '<code class="code-block__list">' +
       `<ul class="code-block__gutter">${list.join('')}</ul>` +
       '<span class="code-block__wrapper">' +
-        `<ul class="${codeClassName}">${content.join('')}</ul>` +
+        `<ul class="${codeClassName}">${codeLineRendered.join('')}</ul>` +
       '</span>' +
       '</code>' +
     '</pre>'
