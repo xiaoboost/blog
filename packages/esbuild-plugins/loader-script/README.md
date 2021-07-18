6df7faea7e63ab59a298519c8b8b867b34202255
xiaoboost <xiaoboost@qq.com>
2021-07-19 00:58:24 +0800

    finish script loader

---
+++ packages/esbuild-plugins/loader-script/README.md
@@ -0,0 +1,3 @@
+# blog/esbuild-loader-script
+
+提供给 esbuild 的`.script.ts`文件的加载器，此加载器将会把此文件的所有代码独立打包。
--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,3 +1,4 @@
 export * from './env';
+export * from './fs';
 export * from './string';
 export * from './sanbox';
