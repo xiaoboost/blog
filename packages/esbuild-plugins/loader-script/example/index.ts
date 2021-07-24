e1a20dacd8b3201a64c5fd3e1a4f427989c03d12
xiaoboost <xiaoboost@qq.com>
2021-07-24 23:01:22 +0800

    finish script loader

---
+++ packages/esbuild-plugins/loader-script/example/index.ts
@@ -0,0 +1,3 @@
+import './test.script';
+
+console.log('outside');
---
+++ packages/esbuild-plugins/loader-script/example/test.script.ts
@@ -0,0 +1,5 @@
+import styles from './test.jss';
+
+const el = document.createElement('div');
+el.innerHTML = '<div>123</div>';
+el.className = styles.classes.box;
