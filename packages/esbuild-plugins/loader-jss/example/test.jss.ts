c7f2fbc01c8959268d8459cffab702a8df9f6577
xiaoboost <xiaoboost@qq.com>
2021-07-26 00:03:36 +0800

    fix loader-jss

--- packages/esbuild-plugins/loader-jss/example/test.jss.ts
+++ packages/esbuild-plugins/loader-jss/example/test.jss.ts
@@ -1,8 +1,12 @@
 import jss from 'jss';
+import preset from 'jss-preset-default';
+
+jss.setup(preset());
 
 export default jss.createStyleSheet({
   box: {
     margin: 0,
     padding: 0,
+    backgroundImage: `url('./example.svg')`,
   },
 });
