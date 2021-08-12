5c0cb87b2588cc6a5684acce17abe6c720c27b33
xiaoboost <xiaoboost@qq.com>
2021-08-13 02:54:22 +0800

    finish layout component jss

--- packages/templates/layout/src/components/footer/index.tsx
+++ packages/templates/layout/src/components/footer/index.tsx
@@ -1,14 +1,13 @@
-import './index.styl';
-
 import React from 'react';
+import styles from './index.jss';
 
 export function Footer() {
   return (
-    <footer className='main-footer'>
+    <footer className={styles.classes.mainFooter}>
       <span>Powered by </span>
-      <a href='https://github.com/xiaoboost' className='main-footer__href'>Xiao</a>
+      <a href='https://github.com/xiaoboost' className={styles.classes.mainFooterHref}>Xiao</a>
       <span> & </span>
-      <a href='https://pages.github.com/' className='main-footer__href'>Github Pages</a>
+      <a href='https://pages.github.com/' className={styles.classes.mainFooterHref}>Github Pages</a>
       <span style={{ fontSize: '110%' }}> © </span>
       <span>Since 2020</span>
     </footer>
