05308c7332b6ebf4a71a383620c11e2e96c221b2
xiaoboost <xiaoboost@qq.com>
2022-05-08 04:30:24 +0800

    other: update version and add pure mark for var

--- packages/tools/builder/src/bundler/utils/log.ts
+++ packages/tools/builder/src/bundler/utils/log.ts
@@ -1,7 +1,23 @@
-const prefix = ' >';
+import createSpinner, { Ora } from 'ora';
+
+const prefix = '>';
+let spinner: Ora | undefined;
 
 export const log = {
   log(...args: any[]) {
     console.log(prefix, ...args);
+  },
+  loadStart(message: string) {
+    spinner = createSpinner({
+      interval: 200,
+      text: message,
+    });
+    spinner.start();
+  },
+  loadEnd() {
+    if (spinner) {
+      spinner.stop();
+      spinner = undefined;
+    }
   },
 };
