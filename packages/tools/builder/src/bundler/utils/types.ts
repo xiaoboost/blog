5faf1f0ad0d4827fa98d336127ab5fc991a05e9b
xiaoboost <xiaoboost@qq.com>
2022-05-22 15:47:03 +0800

    fix: format error print

--- packages/tools/builder/src/bundler/utils/types.ts
+++ packages/tools/builder/src/bundler/utils/types.ts
@@ -4,3 +4,19 @@ export interface FileCache {
   readFile(target: string): FileContent | Promise<FileContent>;
   writeFile(target: string, content: Buffer): void;
 }
+
+export interface Position {
+  line: number;
+  column?: number;
+}
+
+export interface BuildError {
+  message: string;
+  file: string;
+  content: string;
+  label?: string;
+  position: {
+    start: Position;
+    end?: Position;
+  };
+}
