4f03a33806575d97bf8886766e70cc9362e80a54
xiaoboost <xiaoboost@qq.com>
2022-07-28 00:53:13 +0800

    feat: finish component disable method

--- packages/components/scrollbar/src/index.script.ts
+++ packages/components/scrollbar/src/index.script.ts
@@ -1,7 +1,21 @@
 import styles from './index.jss';
 
 import { ScrollBar } from './scrollbar';
+import { getCurrentScriptSrc } from '@blog/shared/web';
 
-Array.from(document.querySelectorAll(`.${styles.classes.scrollbar}`)).forEach((el) => {
-  new ScrollBar(el as HTMLElement);
-});
+function active() {
+  const scrollBars = Array.from(document.querySelectorAll(`.${styles.classes.scrollbar}`)).map(
+    (el) => new ScrollBar(el as HTMLElement),
+  );
+
+  return () => {
+    scrollBars.forEach((bar) => bar.disable());
+  };
+}
+
+if (process.env.NODE_ENV === 'development' && window.Module) {
+  window.Module.install({
+    currentScript: getCurrentScriptSrc(),
+    active,
+  });
+}
