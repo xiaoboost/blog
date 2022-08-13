a35de7d6b92ded206b84ed9febedd0dba88fc2ad
xiaoboost <xiaoboost@qq.com>
2022-08-13 18:38:31 +0800

    feat: finish hmr post

--- packages/shared/src/node/font.ts
+++ packages/shared/src/node/font.ts
@@ -1,19 +1,17 @@
-import Fontmin from 'fontmin';
+// import Fontmin from 'fontmin';
 
 export function getMinFont(fontFile: Buffer, txtSet: string) {
-  return new Promise((resolve, reject) => {
-    const font = new Fontmin();
-
-    font
-      .src(fontFile)
-      .use(Fontmin.ttf2woff2())
-      .run((err, files) => {
-        if (err) {
-          reject(err);
-        }
-
-        // TODO:
-        console.log(files[0]);
-      });
-  });
+  // return new Promise((resolve, reject) => {
+  //   const font = new Fontmin();
+  //   font
+  //     .src(fontFile)
+  //     .use(Fontmin.ttf2woff2())
+  //     .run((err, files) => {
+  //       if (err) {
+  //         reject(err);
+  //       }
+  //       // TODO:
+  //       console.log(files[0]);
+  //     });
+  // });
 }
