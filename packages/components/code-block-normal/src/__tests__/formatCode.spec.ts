bd83d329df39f464d1f025dfc877c9f430535604
xiaoboost <xiaoboost@qq.com>
2022-08-15 01:04:46 +0800

    test: finish format and mini space test

---
+++ packages/components/code-block-normal/src/__tests__/formatCode.spec.ts
@@ -0,0 +1,61 @@
+import { expect } from '@blog/tests';
+import { formatCode } from '../utils';
+
+describe('formatCode', () => {
+  it('basic', () => {
+    const code = `
+    const abc = '123';
+    const abc = '123';
+    const abc = '123';
+    `;
+
+    const result = formatCode(code);
+    expect(result).to.eq(
+      `
+const abc = '123';
+const abc = '123';
+const abc = '123';
+    `.trim(),
+    );
+  });
+  it('have code block', () => {
+    const code = `
+    if (true) {
+      const abc = '123';
+    }
+    `;
+
+    const result = formatCode(code);
+    expect(result).to.eq(
+      `
+if (true) {
+  const abc = '123';
+}
+    `.trim(),
+    );
+  });
+  it('have deep code block', () => {
+    const code = `
+    if (true) {
+      const abc = '123';
+
+      if (true) {
+        const abc2 = '123';
+      }
+    }
+    `;
+
+    const result = formatCode(code);
+    expect(result).to.eq(
+      `
+if (true) {
+  const abc = '123';
+
+  if (true) {
+    const abc2 = '123';
+  }
+}
+    `.trim(),
+    );
+  });
+});
