8127543be17310982aeaff9b8c9d016379adb11e
xiaoboost <xiaoboost@qq.com>
2022-08-15 01:04:46 +0800

    test: finish format and mini space test

---
+++ packages/components/code-block-normal/src/__tests__/getMinSpaceWidth.spec.ts
@@ -0,0 +1,53 @@
+import { expect } from '@blog/tests';
+import { getMinSpaceWidth } from '../utils';
+
+describe('getMinSpaceWidth', () => {
+  it('default space', () => {
+    const code = `
+const abc = '123';
+const abc = '123';
+const abc = '123';
+    `;
+
+    expect(getMinSpaceWidth(code)).to.eq(2);
+  });
+  it('two space', () => {
+    const code = `
+if (true) {
+  const abc = '123';
+
+  if (true) {
+    const abc2 = '123';
+  }
+}
+    `;
+
+    expect(getMinSpaceWidth(code)).to.eq(2);
+  });
+  it('four space', () => {
+    const code = `
+if (true) {
+    const abc = '123';
+
+    if (true) {
+        const abc2 = '123';
+    }
+}
+    `;
+
+    expect(getMinSpaceWidth(code)).to.eq(4);
+  });
+  it('have tow mode space, select the small one', () => {
+    const code = `
+if (true) {
+    const abc = '123';
+
+  if (true) {
+      const abc2 = '123';
+  }
+}
+    `;
+
+    expect(getMinSpaceWidth(code)).to.eq(2);
+  });
+});
