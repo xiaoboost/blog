4f71a85ef210a1be274f957d1fd1ed34be4f1c54
xiaoboost <xiaoboost@qq.com>
2022-11-22 02:01:05 +0800

    feat: finish runner

--- packages/builder/context/src/styles.ts
+++ packages/builder/context/src/styles.ts
@@ -1,8 +1,16 @@
-import type Jss from 'jss';
+import Jss from 'jss';
+import preset from 'jss-preset-default';
 
-export const JssKey = '_JSS';
+Jss.setup(preset());
 
-/** 虚拟 jss 变量 */
+const JssKey = '_JSS';
+
 declare const _JSS: typeof Jss;
 
+/** 缓存变量上下文 */
+export const StyleContext = {
+  [JssKey]: Jss,
+};
+
+/** Jss 构造器 */
 export const jss = _JSS;
