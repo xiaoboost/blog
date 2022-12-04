0c12b41cd53eb2dad8a5aa216835baeb7307aae2
xiaoboost <xiaoboost@qq.com>
2022-12-04 22:38:19 +0800

    fix(core): finish basic watcher

--- packages/builder/core/src/bundler/source/index.ts
+++ packages/builder/core/src/bundler/source/index.ts
@@ -1,1 +1,21 @@
-console.log('hello');
+import { defineUtils, forEach, forOnce, waitReady, callHook } from '@blog/context/runtime';
+
+forOnce(() => {
+  console.log('forOnce');
+});
+
+forEach((runtime) => {
+  console.log('forEach');
+
+  runtime.hooks.beforeStart.tap('test', () => {
+    console.log('beforeStart');
+  });
+});
+
+console.log('start');
+
+export default async () => {
+  await waitReady;
+  await callHook('beforeStart');
+  console.log('end');
+};
