c83276714a35361cb16abe8da0b8023f2ab0835e
xiaoboost <xiaoboost@qq.com>
2022-12-30 23:00:17 +0800

    feat(core): resolve plugin init

--- packages/builder/core/src/bundler/bridge.ts
+++ packages/builder/core/src/bundler/bridge.ts
@@ -5,11 +5,23 @@ export const BridgePlugin = (bundler: Bundler): Plugin => {
   return {
     name: 'bridge-plugin',
     setup: (esbuild: PluginBuild) => {
-      esbuild.onResolve({ filter: /.*/ }, (resolveArgs) => {
-        return bundler.hooks.resolve.promise(resolveArgs) as any;
+      esbuild.onResolve({ filter: /.*/ }, async (resolveArgs) => {
+        const result = await bundler.hooks.resolve.promise(resolveArgs);
+
+        if (result) {
+          await bundler.hooks.resolveResult.promise(result);
+        }
+
+        return result ?? null;
       });
-      esbuild.onLoad({ filter: /.*/ }, (loadArgs) => {
-        return bundler.hooks.load.promise(loadArgs) as any;
+      esbuild.onLoad({ filter: /.*/ }, async (loadArgs) => {
+        const result = await bundler.hooks.load.promise(loadArgs);
+
+        if (result) {
+          await bundler.hooks.loadResult.promise(result);
+        }
+
+        return result ?? null;
       });
     },
   };
