60ee69ca27832af14c1a6b0700a85ba876b2e011
xiaoboost <xiaoboost@qq.com>
2023-01-08 00:48:33 +0800

    fix(core): fix build error

--- packages/builder/core/src/bundler/source/index.ts
+++ packages/builder/core/src/bundler/source/index.ts
@@ -1,12 +1,11 @@
 import posts from '@blog/posts';
 
 import type { AssetData, ListRenderData } from '@blog/types';
-import { join } from 'path';
 import { cut } from '@xiao-ai/utils';
-import { callHook, waitReady, builderOptions } from '@blog/context/runtime';
+import { callHook, waitReady } from '@blog/context/runtime';
 import { componentReady } from './component';
-import { renderPost, getPostUrlMap } from './post';
-import { getIndexPathname, renderListPage } from './list';
+import { renderPost, getPostUrlMap, getPostAssetPath } from './post';
+import { renderListPage, getIndexUrlPath, getIndexAssetPath } from './list';
 import { pageConfig } from '../../constant';
 
 export default async function main() {
@@ -20,11 +19,12 @@ export default async function main() {
   const postUrlMap = getPostUrlMap(posts);
   await callHook('afterPostUrl', postUrlMap);
 
+  debugger;
   // 生成文章页面
   for (const post of posts) {
     await callHook('beforeEachPost', post);
     const asset: AssetData = {
-      path: join(builderOptions.publicPath, post.data.pathname),
+      path: getPostAssetPath(post),
       content: Buffer.from(renderPost(post)),
     };
     await callHook('afterEachPost', asset);
@@ -37,13 +37,14 @@ export default async function main() {
   for (let i = 0; i < lists.length; i++) {
     const data: ListRenderData = {
       index: i,
-      pathname: getIndexPathname(i),
+      count: lists.length,
+      pathname: getIndexUrlPath(i),
       posts: lists[i],
     };
 
     await callHook('beforeEachList', data);
     const asset: AssetData = {
-      path: data.pathname,
+      path: getIndexAssetPath(i),
       content: Buffer.from(renderListPage(data)),
     };
     await callHook('afterEachList', asset);
