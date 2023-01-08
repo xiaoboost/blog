8fac36f0805630f92f8f705d77f1d1c6f8812b09
xiaoboost <xiaoboost@qq.com>
2023-01-09 00:43:19 +0800

    fix(builder): delete old builder and fix post time error

--- packages/tools/builder/src/bundler/context/global.ts
+++
@@ -1,9 +0,0 @@
-const globalVarMap = new Map<string, any>();
-
-export function setGlobalVar(key: string, val: any) {
-  globalVarMap.set(key, val);
-}
-
-export function getGlobalVar<T = any>(key: string): T | undefined {
-  return globalVarMap.get(key);
-}
