bac03c7bd53b06a854f1854966cb4c179bc8ebde
xiaoboost <xiaoboost@qq.com>
2023-02-19 23:45:49 +0800

    feat(core): type checker worker init

---
+++ packages/builder/core/src/plugins/type-checker/server/files.ts
@@ -0,0 +1,43 @@
+import type * as ts from 'typescript';
+import { getScriptSnapshot } from './utils';
+
+export interface FileData {
+  /** 文件版本 */
+  version: number;
+  /** 文件代码快照 */
+  snapshot: ts.IScriptSnapshot;
+}
+
+/** 文件缓存 */
+export class FileMemory extends Map<string, FileData> {
+  getNames() {
+    return Array.from(this.keys());
+  }
+
+  getVersion(file: string) {
+    return this.get(file)?.version ?? 0;
+  }
+
+  getSnapshot(file: string) {
+    return this.get(file)?.snapshot;
+  }
+
+  setScript(file: string, code: string) {
+    if (!this.has(file)) {
+      this.set(file, {
+        version: 1,
+        snapshot: getScriptSnapshot(code),
+      });
+    } else {
+      const oldData = this.get(file)!;
+      const oldCode = oldData.snapshot.getText(0, Infinity);
+
+      if (oldCode !== code) {
+        this.set(file, {
+          version: oldData.version++,
+          snapshot: getScriptSnapshot(code),
+        });
+      }
+    }
+  }
+}
--- packages/builder/core/src/plugins/type-checker/types.ts
+++ packages/builder/core/src/plugins/type-checker/types.ts
@@ -1,32 +1,19 @@
 export interface EventData {
-  /** 事件编号 */
-  id: number;
   /** 调用方法名称 */
   name: EventName;
   /** 方法参数 */
   params: any[];
-  /** 返回数据 */
-  return?: any;
-  /** 运行错误 */
-  error?: EventError;
-}
-
-export interface EventError {
-  /** 错误名称 */
-  name: string;
-  /** 错误信息 */
-  message: string;
 }
 
 export enum EventName {
   /** 文件变更 */
-  FileChanged,
-  /** 新文件 */
-  FileCreated,
+  FilesChanged,
   /** 开始运行服务器 */
   StartServer,
   /** 获取代码类型诊断 */
   GetTsDiagnostics,
+  /** 停止服务器 */
+  Dispose,
 }
 
 export interface TypeScriptConfig {
