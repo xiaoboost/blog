da283c3faebddf992c0bbe24ac2e280ed71939e2
xiaoboost <xiaoboost@qq.com>
2023-02-20 02:00:35 +0800

    feat(core): finish basic type checker

--- packages/builder/core/src/plugins/type-checker/index.ts
+++ packages/builder/core/src/plugins/type-checker/index.ts
@@ -10,19 +10,20 @@ const pluginName = 'type-checker';
 export const TypeChecker = (opt?: TypeCheckerOptions): BuilderPlugin => ({
   name: pluginName,
   apply(builder) {
-    const worker = new WorkerController(join(__dirname, './server/worker.js'));
+    const worker = new WorkerController(join(__dirname, './server/index.worker.js'));
 
     builder.hooks.afterInitialized.tap(pluginName, () => {
-      worker
-        .send({
-          name: EventName.StartServer,
-          params: [builder.options.root, opt],
-        })
-        .then(() =>
-          worker.send({
-            name: EventName.GetTsDiagnostics,
-          }),
-        );
+      worker.send({
+        name: EventName.StartServer,
+        params: [
+          {
+            basePath: opt?.basePath ?? builder.options.root,
+            configOverwrite: opt?.configOverwrite ?? {},
+            configFile: opt?.configFile ?? 'tsconfig.json',
+            typescriptPath: opt?.typescriptPath ?? require.resolve('typescript'),
+          },
+        ],
+      });
     });
 
     builder.hooks.filesChange.tap(pluginName, (files) => {
