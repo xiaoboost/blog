02ddd6cf2c5e16bbe4795e87fe5850ec39900980
xiaoboost <xiaoboost@qq.com>
2024-04-06 21:42:45 +0800

    chore: upgrade tsconfig

--- packages/utils/node/src/path.ts
+++ packages/utils/node/src/path.ts
@@ -1,5 +1,4 @@
 import path from 'path';
-import { platform } from 'process';
 import { AnyObject } from '@xiao-ai/utils';
 
 /** 标准化路径 */
@@ -47,21 +46,7 @@ export function getPathFormatter(formatStr: string) {
 }
 
 export function isRootDirectory(dirPath: string) {
-  // 规范化路径
   const normalizedPath = path.resolve(dirPath);
-  // 分析路径
-  const parsedPath = path.parse(normalizedPath);
-
-  // 对于 Windows 系统，检查根目录是否为空
-  if (platform === 'win32' && parsedPath.root === '') {
-    return true;
-  }
-
-  // 对于 Unix-like 系统，检查规范化后的路径是否和原始路径相同
-  if (parsedPath.root === '/' && normalizedPath === dirPath) {
-    return true;
-  }
-
-  // 其他情况都不是根路径
-  return false;
+  // 上级路径和当前路径相等
+  return path.dirname(normalizedPath) === normalizedPath;
 }
