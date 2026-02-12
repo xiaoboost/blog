bf4e089ce313c7fabf04e4f8720b92555aadf70f
xiaoboost <xiaoboost@qq.com>
2026-02-13 02:21:18 +0800

    feat: finish font bucket

--- packages/templates/layout/src/styles/font.jss.ts
+++ packages/templates/layout/src/styles/font.jss.ts
@@ -1,12 +1,7 @@
 import { createFontFaceStyles, mergeStyles, CustomFont } from '@blog/styles';
-
-function getFontPath(name: string, subName?: string) {
-  return subName ? `../assets/font/${name}/${name}-${subName}` : `../assets/font/${name}/${name}`;
-}
+import { getFontPath } from '../utils';
 
 export default mergeStyles(
-  // 标题字体
-  createFontFaceStyles(CustomFont.Dancing, 'normal', 'normal', getFontPath('dancing')),
   // 英文正文
   createFontFaceStyles(CustomFont.Lato, 'normal', 'bold', getFontPath('Lato', 'Bold')),
   createFontFaceStyles(CustomFont.Lato, 'italic', 'normal', getFontPath('Lato', 'Italic')),
